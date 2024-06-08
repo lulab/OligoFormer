a97e4baa23df482703e5c3aa37f71113b6b67548
byl18 <65812792+byl18@users.noreply.github.com>
2024-06-08 18:55:08 +0800

    Update loader.py

--- scripts/loader.py
+++ scripts/loader.py
@@ -7,26 +7,14 @@ from sklearn.preprocessing import LabelEncoder
 import torch
 
 nucleic_char = ['A', 'U', 'C', 'G', 'X'] # 
-structure_char = ['.', '(', ')']
-
-# ACGU 1234
 
 enc_protein = OneHotEncoder().fit(np.array(nucleic_char).reshape(-1, 1))
-enc_structure =  LabelEncoder().fit(np.array(structure_char))
 def sequence_OneHot(x):
     return enc_protein.transform(np.array(x).reshape(-1, 1)).toarray().T
 
-def structure_Label(x):
-    return enc_structure.transform(np.array(x).reshape(-1, 1)).T
-
-def Embed(RNA):
-    nucleotides = 'ACGU' 
-    char_to_int = dict((c, i + 1 ) for i, c in enumerate(nucleotides))
-    return [char_to_int[i] for i in RNA]
-
 
 class data_process_loader(data.Dataset):
-    def __init__(self, df_index, labels, y, df, dataset):
+    def __init__(self, df_index, labels, y, df, dataset,root):
         'Initialization'
         self.labels = labels
         self.df_index = df_index
@@ -34,7 +22,8 @@ class data_process_loader(data.Dataset):
         self.df = df
         self.dataset = dataset
         self.siRNA_ref = {}
-        with open('./data/fasta/' + self.dataset + '_siRNA.fa') as f:
+        self.root = root
+        with open(root + '/fasta/' + self.dataset + '_siRNA.fa') as f:
             for line in f:
                 if line.startswith('>'):
                     name = line.strip().replace('>','')
@@ -42,7 +31,7 @@ class data_process_loader(data.Dataset):
                     seq = line.strip()
                     self.siRNA_ref[seq] = name
         self.mRNA_ref = {}
-        with open('./data/fasta/' + self.dataset + '_mRNA.fa') as f:
+        with open(root + '/fasta/' + self.dataset + '_mRNA.fa') as f:
             for line in f:
                 if line.startswith('>'):
                     name = line.strip().replace('>','')
@@ -67,10 +56,10 @@ class data_process_loader(data.Dataset):
         mRNA = np.expand_dims(mRNA_seq, axis=2).transpose([2, 1, 0])
         # siRNA RNA-FM
         siRNA_seq = self.df.iloc[index]['siRNA']
-        siRNA_FM = np.load('./data/RNAFM/' + self.dataset + '_siRNA/representations/'+ str(self.siRNA_ref[siRNA_seq]) + '.npy')
+        siRNA_FM = np.load(self.root + '/RNAFM/' + self.dataset + '_siRNA/representations/'+ str(self.siRNA_ref[siRNA_seq]) + '.npy')
         # mRNA RNA-FM
         mRNA_seq = self.df.iloc[index]['mRNA']
-        mRNA_FM = np.load('./data/RNAFM/' + self.dataset + '_mRNA/representations/' + str(self.mRNA_ref[mRNA_seq])+'.npy') 
+        mRNA_FM = np.load(self.root + '/RNAFM/' + self.dataset + '_mRNA/representations/' + str(self.mRNA_ref[mRNA_seq])+'.npy') 
         td = self.df.iloc[index]['td']
         td = torch.tensor([float(i) for i in td.split(',')])
         return  siRNA, mRNA, siRNA_FM, mRNA_FM, label, y, td
@@ -104,12 +93,12 @@ class data_process_loader_infer(data.Dataset):
     def __getitem__(self, index):
         index = self.df_index[index]
         # siRNA
-        siRNA_seq = self.df.iloc[index]['siRNA'].replace('T','U')
+        siRNA_seq = self.df.iloc[index]['siRNA']
         siRNA_seq = [*siRNA_seq]
         siRNA_seq = sequence_OneHot(siRNA_seq)
         siRNA = np.expand_dims(siRNA_seq, axis=2).transpose([2, 1, 0])
         # mRNA
-        mRNA_seq = self.df.iloc[index]['mRNA'].replace('T','U')
+        mRNA_seq = self.df.iloc[index]['mRNA']
         mRNA_seq = [*mRNA_seq]
         mRNA_seq = sequence_OneHot(mRNA_seq)
         mRNA = np.expand_dims(mRNA_seq, axis=2).transpose([2, 1, 0])
