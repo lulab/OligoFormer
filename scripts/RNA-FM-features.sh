acab0dd4571e3c35b92ec94f09990c37659ad502
byl18 <65812792+byl18@users.noreply.github.com>
2024-06-08 16:17:29 +0800

    Update RNA-FM-features.sh

--- scripts/RNA-FM-features.sh
+++ scripts/RNA-FM-features.sh
@@ -3,7 +3,7 @@ path=$(which python)
 
 path=(${path//\/envs/ }) 
 cd ./RNA-FM/redevelop
-for file in Hu_siRNA Hu_mRNA new_siRNA new_mRNA Taka_siRNA Taka_mRNA 
+for file in Hu_siRNA Hu_mRNA Mix_siRNA Mix_mRNA Taka_siRNA Taka_mRNA 
 do
 $path/envs/RNA-FM/bin/python launch/predict.py --config="pretrained/extract_embedding.yml" \
 --data_path=../../data/fasta/$file.fa --save_dir=../../data/RNAFM/$file \
