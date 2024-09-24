5d9c9fe24eb949824912232c883d7124e9611e8c
byl18 <65812792+byl18@users.noreply.github.com>
2024-09-24 14:18:11 +0800

    Add files via upload

--- scripts/RNA-FM-features.sh
+++ scripts/RNA-FM-features.sh
@@ -1,11 +1,8 @@
 #!/bin/bash
-path=$(which python)
-
-path=(${path//\/envs/ }) 
 cd ./RNA-FM/redevelop
 for file in Hu_siRNA Hu_mRNA Mix_siRNA Mix_mRNA Taka_siRNA Taka_mRNA 
 do
-$path/envs/RNA-FM/bin/python launch/predict.py --config="pretrained/extract_embedding.yml" \
+python launch/predict.py --config="pretrained/extract_embedding.yml" \
 --data_path=../../data/fasta/$file.fa --save_dir=../../data/RNAFM/$file \
 --save_frequency 1 --save_embeddings
 done
